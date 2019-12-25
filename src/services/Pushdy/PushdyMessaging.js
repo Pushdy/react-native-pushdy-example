06dcec05a773a9fce61fccae6a3ba202fe7032ff
luatnd <luatnd@mobiletech.vn>
2019-12-25 17:39:16 +0700

    Handle initial push notification Cast notification generic object to PushdyNotification class

--- src/services/Pushdy/PushdyMessaging.js
+++ src/services/Pushdy/PushdyMessaging.js
@@ -5,7 +5,7 @@
  * You can use this file as Pushdy wrapper service for you app
  * Replace NavigationService by your navigation service
  */
-import Pushdy from 'react-native-pushdy'
+import Pushdy, { PushdyNotification } from 'react-native-pushdy'
 import { Platform, Alert, NativeEventEmitter } from "react-native";
 import OpenAppSettings from 'react-native-app-settings';
 
@@ -28,11 +28,11 @@ class PushdyMessaging {
     Pushdy.setTimeout(5000);
 
     const [msg, x2num] = await Pushdy.sampleMethod('Hello from JS with', 500);
-    console.log('{register} msg, x2num: ', msg, x2num);
+    this.debug && this.log.debug('{register} msg, x2num: ', msg, x2num);
 
-    if (Platform.OS === 'ios') {
-      await Pushdy.ios_registerForPushNotification();
-    }
+    // You must ensure permission before you can receive push
+    // await Pushdy.ios_registerForPushNotification()
+    await this.ensurePermission();
 
     // Remember to subscribe first
     const _this = this;
@@ -46,13 +46,14 @@ class PushdyMessaging {
 
     // After setting up subscribers, you can continue to work with Pushdy
     Pushdy.getDeviceToken().then((deviceToken) => {
-      // console.log('{PushdyMessaging} deviceToken: ', deviceToken);
       if (deviceToken) {
         this.handleTokenUpdated(deviceToken);
       } else {
         this.log.info('deviceToken is empty: ', deviceToken);
       }
     });
+
+    this.handleInitialNotification();
   }
 
   unregister() {
@@ -67,8 +68,6 @@ class PushdyMessaging {
    * @returns {Promise<void>}
    */
   async ensurePermission(showAccquireOSSettingPopup = false) {
-    return;
-
     const enabled = await Pushdy.isNotificationEnabled();
     if (enabled) {
       // user has permissions
@@ -78,7 +77,9 @@ class PushdyMessaging {
       // https://guide.pushdy.com/i/cai-dat-mobile-push/ios#3-cai-dat-sdk-cho-ios-app
       // The OS dialog was show only once => Case 1 bellow
       // If user turn push-noti off => case 2 bellow
-      await Pushdy.ios_registerForPushNotifications();
+      if (Platform.OS === 'ios') {
+        await Pushdy.ios_registerForPushNotification();
+      }
 
       /**
        * Case 1: First time app open => Show a native popup and user can choose Allow / Disallow
@@ -142,41 +143,108 @@ class PushdyMessaging {
 
   /**
    * Notification was opened from
-   * @param notification
-   * @param fromState
+   * @param {PushdyNotification} notification
+   * @param {String} fromState
    */
   onNotificationOpened({notification, fromState}) {
-    console.log('{onNotificationOpened} event: ', {notification, fromState});
-
+    this.debug && this.log.info('{onNotificationOpened} event: ', {notification, fromState});
     this.handleMyAppPushAction(notification, fromState);
   }
 
+  /**
+   * You can handle and show your in-app banner here
+   * If not, PushdySDK for ios already have implemented a default app banner
+   *
+   * @param {PushdyNotification} notification
+   * @param {String} fromState
+   */
   onNotificationReceived({notification, fromState}) {
-    console.log('{onNotificationReceived} event: ', {notification, fromState});
+    this.debug && this.log.info('{onNotificationReceived} event: ', {notification, fromState});
   }
 
   onRemoteNotificationFailedToRegister(event) {
-    console.log('{onRemoteNotificationFailedToRegister} event: ', event);
+    this.debug && this.log.info('{onRemoteNotificationFailedToRegister} event: ', event);
   }
 
   onRemoteNotificationRegistered({}) {}
 
+  /**
+   * When your app is in closed state (not background), incomming notification message will be handled and show by OS, you can find it in the OS's notification center
+   * Then user press the notification, the app will be opened with that notification data, that data called pendingPushNotification
+   *
+   * After JS thread was ready, PushdySDK was ready, you need to check if there is a pendingPushNotification
+   * and handle it
+   *
+   * In some other SDK, pendingPushNotifications also known as initialNotifications
+   */
+  async handleInitialNotification() {
+    // Get the clicked push notification while app is closed
+    const pendingNotification = await Pushdy.getPendingNotification();
+    // this.debug && this.log.info('{handleInitialNotification} pendingNotification: ', pendingNotification);
+
+    if (pendingNotification) {
+      this.handleMyAppPushAction(pendingNotification, "closed");
+    }
+  }
 
   /**
    * ======== App specific behavior ========
    */
   handleMyAppPushAction(notification, fromState) {
-    const action = notification.push_action;
+    this.debug && this.log.info("[handleMyAppPushAction] notification, fromState", notification, fromState);
+
     const data = notification.data;
-    const pushData = data ? data.push_data : {};
+    let pushData = data ? data.push_data : {};
+    if (typeof pushData === 'string') {
+      pushData = JSON.parse(pushData);
+    }
+    const action = data.push_action;
 
     switch (action) {
+      /*
+      POST https://api.pushdi.com/notification with raw body bellow:
+      {
+        "id": "",
+        "status": "waiting",
+        "platforms": ["ios", "android"],
+        "segment_ids": [],
+        "campaign_ids": [],
+        "filters": [
+          {
+            "field": "device_token",
+            "value": "77B7BA310E95397DCADFD6DAC86B919BE17FFCB4466465A26BCA8FCECF4ACF96",
+            "relation": "="
+          }
+        ],
+
+        "priority": "high",
+        "mutable-content": 1,
+
+        "headings": "Anh Vượng dự định bán tivi",
+        "contents": "Hãng xe tỷ phú Phạm Nhật Vượng vừa có thoả thuận lịch sử, lại rò rỉ tin mới về tivi. Hãng xe tỷ phú Phạm Nhật Vượng vừa có thoả thuận lịch sử, lại rò rỉ tin mới về tivi. Hãng xe tỷ phú Phạm Nhật Vượng vừa có thoả thuận lịch sử, lại rò rỉ tin mới về tivi",
+        "image": "https://znews-photo.zadn.vn/w660/Uploaded/cqdhmdxwp/2019_08_14/kuncherry90_67233597_965480387137244_1646091755794003933_n_copy.jpg",
+
+        "data": {
+          "push_action": "nav_to_article_detail",
+          "push_data": {
+              "article_id": 179269
+          },
+
+          "title": "Bão số 6 hướng đi khó lường",
+          "body": "Ít nhất 7 tỉnh thành sẽ bị ảnh hưởng, cần sẵn sàng tinh thần ứng phó",
+          "image": "https://vortex.accuweather.com/adc2010/images/icons-numbered/01-l.png"
+        },
+        "player_ids": [],
+        "type": "mobile_push",
+        "tags": ["test"]
+      }
+       */
       case 'nav_to_article_detail':
         NavigationService.navigate('ArticleDetail', {
           article_id: pushData.article_id,
           ts: +(new Date),
-          title: data.title,
-          body: data.body,
+          title: notification.title,
+          body: notification.body,
         });
         break;
       case 'nav_to_hello_world':
